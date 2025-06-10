70e33cf63bd17d14d834df971b30860fd424502e
Aron Ondieki Onkware <aronondieki045@gmail.com>
2025-06-10 23:30:00 +0300

    Understood the concept

--- server/models.py
+++ server/models.py
@@ -10,12 +10,23 @@ db = SQLAlchemy(metadata=metadata)
 # define a model class by inheriting from db.Model.
 
 
-class Pet(db.Model):
-    __tablename__ = 'pets'
+class Employee(db.Model):
+    __tablename__ = 'employees'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    species = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    salary = db.Column(db.Integer)
 
     def __repr__(self):
-        return f'<Pet {self.id}, {self.name}, {self.species}>'
+        return f'<Employee {self.id}, {self.name}, {self.salary}>'
+
+
+class Department(db.Model):
+    __tablename__ = 'departments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    location = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Department {self.id}, {self.name}, {self.address}>'
